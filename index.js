ec25fb3197c052be317e2c20b211341c9ca49e74
rrashmi642 <0173cs191049@gmail.com>
2025-06-03 12:53:45 +0530

    functions

--- index.js
+++ index.js
@@ -4,3 +4,26 @@ console.log(b);
 let a=[1, 2, 3];
 console.log(a);
 console.log("Hello World");
+
+//functions in js
+
+function sum(a,b){
+return a+b;
+}
+console.log(sum(5,2));
+
+//block scope 
+
+function fun(){
+    let b=9;
+    if(b===9) console.log("inside block", b);
+  }
+  fun();
+  console.log("outside function b =",b);
+
+  //function root
+
+  function sqroot(a){
+    return Math.sqrt(a);
+  }
+  console.log(sqroot(9));
