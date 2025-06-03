7cc2fb3e73a487b25374fd5e6fe41ac909bc4c7c
rrashmi642 <0173cs191049@gmail.com>
2025-06-03 14:26:17 +0530

    Array functions

---
+++ Array.js
@@ -0,0 +1,18 @@
+let arr=["banana", "papaya", "mango"];
+arr.push("Kiwi"); //adds element at end
+console.log(arr);
+
+arr.pop(); //emoves element from end
+console.log(arr);
+
+arr.shift(); //removes element from front
+console.log(arr);
+
+arr.unshift("berries") // adds element at front
+console.log(arr);
+
+
+console.log(arr.indexOf("papaya")); //gives index
+
+if(arr[1]==="banana") console.log(Arr[1]);
+else console.log("No, it's a", arr[1]);
